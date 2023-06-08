c68aead2179d6b29f274f2aa230f75716cf05509
fabiofognani <ffognani@florence-consulting.it>
2023-06-08 17:01:38 +0200

    feat: blog page

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,5 +1,9 @@
 import type { GatsbyConfig } from "gatsby";
 
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
 const config: GatsbyConfig = {
   siteMetadata: {
     title: `My Gatsby Site`,
@@ -9,7 +13,20 @@ const config: GatsbyConfig = {
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: ["gatsby-plugin-postcss"]
+  plugins: [
+    "gatsby-plugin-postcss",
+    `gatsby-plugin-gatsby-cloud`,
+    "gatsby-plugin-image",
+    {
+      resolve: "gatsby-source-contentful",
+      options: {
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        host: process.env.CONTENTFUL_HOST || "cdn.contentful.com",
+        environment: process.env.CONTENTFUL_ENVIRONMENT_ID || "master",
+      },
+    },
+  ]
 };
 
 export default config;
