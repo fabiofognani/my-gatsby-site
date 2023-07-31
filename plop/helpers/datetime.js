ba8c257cebeb2d05dd4a87da1d85c9b0bc55250f
fabiofognani <ffognani@florence-consulting.it>
2023-07-31 16:09:34 +0200

    chore renamed var

--- plop/helpers/datetime.js
+++ plop/helpers/datetime.js
@@ -1,12 +1,12 @@
 module.exports = function (plop) {
   plop.setHelper('datetime', function () {
-    const d = new Date();
+    const date = new Date();
     return [
-      d.getFullYear(),
-      d.getMonth() + 1,
-      d.getDate(),
-      d.getHours(),
-      d.getMinutes(),
+      date.getFullYear(),
+      date.getMonth() + 1,
+      date.getDate(),
+      date.getHours(),
+      date.getMinutes(),
     ].map(digit => String(digit).padStart(2, '0')).join('');
   });
 };
